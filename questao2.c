5daa2011a93c1d2cc20648c65f167831e7a53701
joseavelar648 <144074244+joseavelar648@users.noreply.github.com>
2025-09-20 00:53:24 -0300

    Create questao2.c
    imprime os primeiros numeros naturais de 1 até N

---
+++ questao2.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+  int i;
+  int N ;
+   printf("digite um valor inteiro positivo para N ");
+    scanf("%d", &N);
+    
+      for (i = 1; i<=N; i++){
+        printf("%d\n", i);
+    }
+    return 0;
+}
